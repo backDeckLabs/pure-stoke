35a4f7bb6878013e65298c553f1764c77a04031d
Colin Baxter <colin.baxter@onxmaps.com>
2024-12-21 01:01:52 -0700

    Adding randomized bg logic for LogoMask

--- components/patterns/LogoMask.tsx
+++ components/patterns/LogoMask.tsx
@@ -1,3 +1,4 @@
+import { getRandomIndex } from '@/lib/js-utils'
 import { AspectRatio, Box } from '@chakra-ui/react'
 import React, { FC } from 'react'
 
@@ -6,11 +7,18 @@ export interface LogoMaskProps {
   width?: string
 }
 
-const LogoMask: FC<LogoMaskProps> = ({
-  backgroundAsset = '/assets/videos/sailing-bg-1.mp4',
-  width = '600px',
-}) => {
+const LogoMask: FC<LogoMaskProps> = ({ backgroundAsset, width = '600px' }) => {
   const logoAssetPath = '/assets/logos/Logo_vector_dark.svg'
+
+  const defaultBackgroundAssets = [
+    '/assets/videos/sailing-bg-1.mp4',
+    '/assets/videos/sky-timelapse.mp4',
+    '/assets/videos/surfing-water.mp4',
+    '/assets/videos/ocean-waves-sunset.mp4',
+  ]
+  const defaultAssetIndex = getRandomIndex(defaultBackgroundAssets.length)
+  const backgroundAssetUrl =
+    backgroundAsset || defaultBackgroundAssets[defaultAssetIndex]
 
   return (
     <Box
@@ -36,7 +44,7 @@ const LogoMask: FC<LogoMaskProps> = ({
       <AspectRatio ratio={932 / 268} w="full">
         {/* Use an image or a video as the background */}
         <video autoPlay muted loop>
-          <source src={backgroundAsset} type="video/mp4" />
+          <source src={backgroundAssetUrl} type="video/mp4" />
           Your browser does not support the video tag.
         </video>
       </AspectRatio>
